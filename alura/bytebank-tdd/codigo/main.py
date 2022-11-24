30f90a1ec773c46eff6f04d18c0062a8c2eac2a3
Lucas Soares <75669462+soareslp@users.noreply.github.com>
2022-11-24 12:30:13 -0300

    organizando pastas

--- alura/bytebank-tdd/codigo/main.py
+++ alura/bytebank-tdd/codigo/main.py
@@ -1,5 +1,11 @@
 from codigo.bytebank import Funcionario
 
-lucas = Funcionario("Lucas", "13/03/2000", 1000)
+#lucas = Funcionario("Lucas", "14/10/1998", 1000)
 
-print(lucas.idade())
+#print(lucas.idade())
+
+def teste_idade():
+    lucas_teste = Funcionario('Lucas', "14/10/1998", 2300)
+    print(f'Teste = {lucas_teste.idade()}')
+
+teste_idade()
